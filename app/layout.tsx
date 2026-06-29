e95ff73a741daa2ed681f95e286612c79ce0c795
hayden951 <hayden@harosec.com>
2026-06-29 13:59:08 -0700

    Delete app directory

--- app/layout.tsx
+++
@@ -1,4 +0,0 @@
-export const metadata = { title: "Survey Widget" };
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return <html lang="en"><body>{children}</body></html>;
-}
